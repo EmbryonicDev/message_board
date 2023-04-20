bf45c9af39a8f5b26ef2558b1f9dcc562f1752c3
djdevlam-apple <embryonic.dev@gmail.com>
2023-04-20 10:56:44 +0200

    Write PostTest to make sure the Post model is returning the correct text

--- posts/tests.py
+++ posts/tests.py
@@ -7,3 +7,6 @@ class PostTests(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.post = Post.objects.create(text="This is a test!")
+
+    def test_model_content(self):
+        self.assertEqual(self.post.text, "This is a test!")
